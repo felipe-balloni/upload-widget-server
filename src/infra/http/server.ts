cd13d90f94942de8242f48c8a2c1cfa820c9aeaf
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2025-03-16 17:44:14 -0300

    Add .envs

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,3 +1,4 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 
@@ -7,6 +8,8 @@ server.register(fastifyCors, {
     origin: true,
 })
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+console.log(env.DATABASE_URL)
+
+server.listen({ port: 3333, host: '127.0.0.1' }).then(() => {
     console.log('Server listening on port 3333.')
 })
