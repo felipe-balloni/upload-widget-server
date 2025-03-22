26af1d4600d055f0fae920ad4028eda98b6556ba
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2025-03-22 14:55:26 -0300

    Add swager

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,9 +1,26 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-    server.post('/uploads', () => {
-        return {
-            message: 'ok',
+    server.post(
+        '/uploads',
+        {
+            schema: {
+                summary: 'Upload an image',
+                body: z.object({
+                    name: z.string(),
+                }),
+                response: {
+                    200: z.object({ uploadId: z.string() }),
+                    409: z
+                        .object({ message: z.string() })
+                        .describe('Upload already exists'),
+                },
+                tags: ['Uploads'],
+            },
+        },
+        async (request, reply) => {
+            return reply.status(200).send({ uploadId: '123' })
         }
-    })
+    )
 }
